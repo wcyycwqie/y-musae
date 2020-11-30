fe36f9c517e5e097877bb62db330b141def8cba7
wenchaoyue <584330116@qq.com>
2020-11-30 16:17:02 +0800

    project initial

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-30 16:08:44
+ * @LastEditTime: 2020-11-30 16:13:20
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \y-musae\src\router\index.js
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
@@ -16,7 +24,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
   }
 ]
 
